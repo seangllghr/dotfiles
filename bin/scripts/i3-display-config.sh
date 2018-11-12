1c815d9fe012eca06d9bf57960365460182c0ed1
Sean Gallagher <seangllghr@gmail.com>
2018-11-12 12:37:50 -0500

    Misc. i3 related tweaks

--- bin/scripts/i3-display-config.sh
+++ bin/scripts/i3-display-config.sh
@@ -1,25 +1,28 @@
 #!/bin/bash
 
-CONNECTION=$(xrandr | grep "HDMI-1-1" | cut -d' ' -f2)
+CONNECTION=$(xrandr | grep "HDMI1" | cut -d' ' -f2)
 
 if [ $CONNECTION = "connected" ]; then
-  xrandr --output HDMI-1-1 --left-of eDP-1-1 --auto --primary
-  nitrogen --restore
+  xrandr --output HDMI1 --primary --mode 1920x1080 --pos 0x0 --rotate normal \
+    --output eDPI1 --mode 1920x1080 --pos 1920x0 --rotate normal \
+    --output VIRTUAL1 --off
+  # nitrogen --restore
   # if [ -e $HOME/.config/i3/xautolock.pid ]; then
     # kill $(cat $HOME/.config/i3/xautolock.pid)
   # fi
   # xautolock -detectsleep -time 10 \
     # -locker "i3lock -e -i $HOME/Pictures/multihead.png" &
-  echo $! > $HOME/.config/i3/xautolock.pid
+  # echo $! > $HOME/.config/i3/xautolock.pid
 else
-  xrandr --auto
-  xrandr --output eDP-1-1 --primary
-  nitrogen --restore
+  xrandr --output eDPI1 --primary --mode 1920x1080 --pos 0x0 --rotate normal \
+    --output HDMI1 --off \
+    --output VIRTUAL1 --off
+  # nitrogen --restore
   # if [ -e $HOME/.config/i3/xautolock.pid ]; then
     # kill $(cat $HOME/.config/i3/xautolock.pid)
   # fi
   # xautolock -detectsleep -time 10 \
     # -locker "i3lock -e -i $HOME/Pictures/single_head.png" &
-  echo $! > $HOME/.config/i3/xautolock.pid
+  # echo $! > $HOME/.config/i3/xautolock.pid
 fi
 xset dpms 0 0 600
--- config/i3/multihead-bar.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-MON_EXTERN=$(xrandr | awk '$1=="HDMI-1-1"{print $8}')
-
-if [ $MON_EXTERN = "connected" ]; then
-  echo "bar { output eDP-1-1 }"
-fi
