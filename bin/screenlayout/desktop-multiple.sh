73c98cff8c06a5089dcc581187b35f1b30023e3d
Sean Gallagher <seangllghr@gmail.com>
2019-01-20 10:53:55 -0700

    Script additions and changes

---
+++ bin/screenlayout/desktop-multiple.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+xrandr --output HDMI1 --primary --mode 1920x1080 --pos 0x0 --rotate normal \
+  --output VIRTUAL1 --off --output eDP1 --off
+xrandr --output HDMI1 --primary --mode 1920x1080 --pos 0x0 --rotate normal \
+  --output VIRTUAL1 --off --output eDP1 --mode 1920x1080 --pos 1920x0 \
+  --rotate normal --output VIRTUAL1 --off
--- bin/scripts/i3-display-config.sh
+++ bin/scripts/i3-display-config.sh
@@ -3,26 +3,8 @@
 CONNECTION=$(xrandr | grep "HDMI1" | cut -d' ' -f2)
 
 if [ $CONNECTION = "connected" ]; then
-  xrandr --output HDMI1 --primary --mode 1920x1080 --pos 0x0 --rotate normal \
-    --output eDPI1 --mode 1920x1080 --pos 1920x0 --rotate normal \
-    --output VIRTUAL1 --off
-  # nitrogen --restore
-  # if [ -e $HOME/.config/i3/xautolock.pid ]; then
-    # kill $(cat $HOME/.config/i3/xautolock.pid)
-  # fi
-  # xautolock -detectsleep -time 10 \
-    # -locker "i3lock -e -i $HOME/Pictures/multihead.png" &
-  # echo $! > $HOME/.config/i3/xautolock.pid
+  exec ~/.screenlayout/desktop-multiple.sh
 else
-  xrandr --output eDPI1 --primary --mode 1920x1080 --pos 0x0 --rotate normal \
-    --output HDMI1 --off \
-    --output VIRTUAL1 --off
-  # nitrogen --restore
-  # if [ -e $HOME/.config/i3/xautolock.pid ]; then
-    # kill $(cat $HOME/.config/i3/xautolock.pid)
-  # fi
-  # xautolock -detectsleep -time 10 \
-    # -locker "i3lock -e -i $HOME/Pictures/single_head.png" &
-  # echo $! > $HOME/.config/i3/xautolock.pid
+  exec ~/.screenlayout/laptop.sh
 fi
 xset dpms 0 0 600
