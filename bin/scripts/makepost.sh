eab65a5b583d7238a414499276a1b1c50ffd6274
Sean Gallagher <seangllghr@gmail.com>
2019-11-09 03:05:59 -0500

    new script. Probably could be a zsh alias. Some config.

--- bin/scripts/makepost.sh
+++ bin/scripts/makepost.sh
@@ -4,8 +4,8 @@ if [ $# -eq 0 ]; then
     echo 'Error: Supply a markdown source.'
 elif [ $# -eq 1 ]; then
     if [ $(echo $1 | cut -d'.' -f 2) == 'md' ]; then
-        pandoc --to=html --wrap=preserve $1 | xsel -ip
-        echo 'Post copied to PRIMARY'
+        pandoc --to=html --wrap=preserve $1 | xsel --input --primary
+        echo 'Post copied to PRIMARY.'
     else
         echo 'Error: expected Markdown source.'
     fi
@@ -17,9 +17,15 @@ elif [ $# -eq 2 ]; then
                inotifywait -e close_write,moved_to,create -m $2 |
                    while read -r directory events filename; do
                        pandoc --to=html --wrap=preserve $2 | xsel -ip
-                       echo 'Post copied to Primary'
+                       echo 'Post copied to PRIMARY.'
                    done
                ;;
+           '-d') : ;&
+           '--debug')
+               exec 5>&1
+               output=$(pandoc --to=html --wrap=preserve $2 | tee /dev/fd/5)
+               echo $output | xsel --input --primary
+               echo "Post copied to PRIMARY."
        esac
     else
         echo 'Error: expected Markdown source.'
---
+++ bin/scripts/term-dimensions.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+echo "$(tput cols)x$(tput lines)"
