d3828da706edc51bb798a71e9be89261cccf9229
Sean Gallagher <seangllghr@gmail.com>
2019-11-20 00:23:08 -0500

    Make sure all changes are pushed for NewAsgard install

--- bin/scripts/i3-display-config.sh
+++ bin/scripts/i3-display-config.sh
@@ -3,8 +3,8 @@
 connection=$(xrandr | grep "HDMI1" | cut -d' ' -f2)
 
 if [ $connection = "connected" ]; then
-  # exec ~/.screenlayout/desktop-multiple.sh # Uncomment for extended displays
-  exec ~/.screenlayout/desktop-single.sh # Uncomment for single desktop
+  exec ~/.screenlayout/desktop-multiple.sh # Uncomment for extended displays
+  # exec ~/.screenlayout/desktop-single.sh # Uncomment for single desktop
 else
   exec ~/.screenlayout/laptop.sh
 fi
