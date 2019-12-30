3cde36fb3654138e3b71dec316d0039142dfb18e
Sean Gallagher <seangllghr@gmail.com>
2019-12-30 18:41:30 -0500

    More robust CapsLock testing in swapescape

--- bin/swapescape.sh
+++ bin/swapescape.sh
@@ -4,8 +4,10 @@ if [ $1 = "-s" ]
 then
   setxkbmap -option caps:swapescape
   setxkbmap -option shift:both_capslock_cancel
-  sleep 0.125
-  xdotool key Caps_Lock
+  sleep 0.25
+  if [ $(xset q | grep Caps | awk '{print $4}') = "on" ]; then
+    xdotool key Caps_Lock
+  fi
 else
   setxkbmap -option
 fi
