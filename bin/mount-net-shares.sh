a889ffdc7d091fc339c6f9c74815b547542f1c9d
Sean Gallagher <seangllghr@gmail.com>
2019-12-30 12:37:09 -0500

    New startup and mount scripts; MarbleMouse config

---
+++ bin/mount-net-shares.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+test_network () {
+    if nmcli connection | grep "wlp12s0b1">&/dev/null; then
+        if [ $(nmcli connection | grep "wlp12s0b1" | awk '{print $1}') = "Yggdrasil" ]; then
+            return 0
+        else
+            return 1
+        fi
+    else
+        return 1
+    fi
+}
+
+wait_for_network () {
+    timer=120
+    while [ $timer -gt 0 ]; do
+        if test_network; then
+            return 0
+        fi
+        echo -ne "Home network not detected. Waiting $timer seconds.\033[0K\r"
+        sleep 1
+        : $((timer--))
+    done
+    return 1
+}
+
+wait_for_server () {
+    timer=15
+    while [ $timer -gt 0 ]; do
+        if ! ping -c 1 -w 1 asgard>&/dev/null; then
+            echo -ne "Asgard is not online. Waiting $timer seconds.\033[0K\r"
+            : $((timer--))
+        else
+            return 0
+        fi
+    done
+    return 1
+}
+
+sleep 30
+if wait_for_network; then
+    echo "Home network found. Attempting to mount network shares."
+    notify-send -u low -t 3000 "Home network found. Attempting to mount network shares."
+    if wait_for_server; then
+        mount /home/sean/Documents
+        mount /home/sean/Downloads
+        notify-send -u low -t 3000 "Mount successful."
+    else
+        notify-send -u normal -t 5000 "Asgard is not online. Mount failed."
+    fi
+else
+    echo "Home network not found. Cannot mount network shares."
+fi
---
+++ config/i3/local-startup_Jotunheim.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# This script runs any configuration particular to Jotunheim, my workstation
+
+mount-net-shares
