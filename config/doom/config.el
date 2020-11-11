aca2315adb705c0f0d82e8df5328b6e3898c609f
Sean Gallagher <seangllghr@gmail.com>
2020-11-11 15:27:17 -0500

    changed font configuration

--- config/doom/config.el
+++ config/doom/config.el
@@ -21,8 +21,14 @@
 ;; font string. You generally only need these two:
 ;; (setq doom-font (font-spec :family "monospace" :size 12 :weight 'semi-light)
 ;;       doom-variable-pitch-font (font-spec :family "sans" :size 13))
-(setq doom-font (font-spec :family "Fira Code" :size 16 :weight 'light)
-      doom-variable-pitch-font (font-spec :family "Noto Sans" :size 16 :weight 'regular))
+(setq doom-font (font-spec
+                 :family "Fira Code"
+                 :size 16
+                 )
+      doom-variable-pitch-font (font-spec
+                                :family "Latin Modern Roman"
+                                :size 18
+                                ))
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
