9465b05a7f5436e5b9157fa0078e2cc58102db76
Sean Gallagher <seangllghr@gmail.com>
2020-12-01 16:03:01 -0500

    enable edit-server for web textareas

--- config/doom/config.el
+++ config/doom/config.el
@@ -68,6 +68,24 @@
   (remove-hook 'window-configuration-change-hook #'golden-ratio)
   (add-hook 'doom-switch-window-hook #'golden-ratio))
 
+;; Edit with Emacs edit server
+(use-package! edit-server
+  :ensure t
+  :commands edit-server-start
+  :init (if after-init-time
+            (edit-server-start)
+          (add-hook 'after-init-hook
+                    #'(lambda() (edit-server-start))))
+  :config (setq edit-server-new-frame-alist
+                '((name . "Edit with Emacs FRAME")
+                  (top . 200)
+                  (left . 200)
+                  (width . 80)
+                  (height . 25)
+                  (minibuffer . t)
+                  (menu-bar-lines . t)
+                  (window-system . x))))
+
 (custom-set-faces!
   '(doom-modeline-buffer-modified :foreground "orange"))
 
