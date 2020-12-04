fe27e27bd614576b4fc058410b6edf628fc94b58
Sean Gallagher <sean.gallagher2@snhu.edu>
2020-12-04 15:30:25 -0500

    fix java editing
    Apparently `+lsp` and `+meghanada` are mutually exclusive. Since the
    former actually seems to work, we'll use that.

--- config/doom/init.el
+++ config/doom/init.el
@@ -128,7 +128,7 @@
        ;;hy                ; readability of scheme w/ speed of python
        ;;idris             ;
        json                ; At least it ain't XML
-       (java +meghanada +lsp)   ; the poster child for carpal tunnel syndrome
+       (java +lsp)   ; the poster child for carpal tunnel syndrome
        javascript          ; all(hope(abandon(ye(who(enter(here))))))
        ;;julia             ; a better, faster MATLAB
        ;;kotlin            ; a better, slicker Java(Script)
