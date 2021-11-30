5c4633a012b526df7a7bd501c5e460118c481b2f
Sean Gallagher <seangllghr@gmail.com>
2021-11-29 19:52:44 -0500

    add js/ts lsp support

--- config/doom/init.el
+++ config/doom/init.el
@@ -129,7 +129,7 @@
        ;;idris             ;
        json                ; At least it ain't XML
        (java +lsp)   ; the poster child for carpal tunnel syndrome
-       javascript          ; all(hope(abandon(ye(who(enter(here))))))
+       (javascript +lsp)          ; all(hope(abandon(ye(who(enter(here))))))
        ;;julia             ; a better, faster MATLAB
        ;;kotlin            ; a better, slicker Java(Script)
        (latex +lsp)        ; writing papers in Emacs has never been so fun
