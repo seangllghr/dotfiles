b0be1439b4a26c6e0d132b1b1dc5f704f8a8ed54
Sean Gallagher <seangllghr@gmail.com>
2022-08-18 15:24:52 -0400

    add/fix docstrings for better python

--- config/qtile/customact.py
+++ config/qtile/customact.py
@@ -1,3 +1,5 @@
+"""Custom functions for my qtile config."""
+
 from libqtile import qtile
 from libqtile.log_utils import logger
 from libqtile.lazy import lazy
@@ -5,7 +7,7 @@ from libqtile.widget.prompt import AbstractCompleter
 
 @lazy.function
 def add_named_section(qtile, widget='prompt'):
-    '''Prompt user for section name, then add it'''
+    """Prompt user for section name, then add it."""
     try:
         promptbox = qtile.widgets_map[widget]
         promptbox.start_input(
@@ -17,7 +19,7 @@ def add_named_section(qtile, widget='prompt'):
 
 @lazy.function
 def del_named_section(qtile, widget='prompt'):
-    '''Prompt user for section name, then delete it'''
+    """Prompt user for section name, then delete it."""
     try:
         promptbox = qtile.widgets_map[widget]
         promptbox.start_input(
@@ -28,16 +30,14 @@ def del_named_section(qtile, widget='prompt'):
         logger.exception(f'No widget named "{widget}" present')
 
 def move_window_to_screen(qtile, window, screen):
-    """Moves a window to a screen and focuses it, allowing you to move it
-    further if you wish."""
+    """Move window to a screen and focus it."""
     window.togroup(screen.group.name)
     qtile.focus_screen(screen.index)
     screen.group.focus(window, True)
 
 @lazy.function
 def move_window_to_prev_screen(qtile):
-    """Moves a window to the previous screen. Loops around the beginning and
-    end."""
+    """Move window to the previous screen."""
     index = qtile.current_screen.index
     index = index - 1 if index > 0 else len(qtile.screens) - 1
     move_window_to_screen(qtile, qtile.current_window, qtile.screens[index])
@@ -45,8 +45,7 @@ def move_window_to_prev_screen(qtile):
 
 @lazy.function
 def move_window_to_next_screen(qtile):
-    """Moves a window to the next screen. Loops around the beginning and
-    end."""
+    """Move window to the next screen."""
     index = qtile.current_screen.index
     index = index + 1 if index < len(qtile.screens) - 1 else 0
     move_window_to_screen(qtile, qtile.current_window, qtile.screens[index])
